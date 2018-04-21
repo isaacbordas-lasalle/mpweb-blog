09329537c84e499d61f9367c0f585e547b6e0000
Isaac <isaac.bordas@gmail.com>
2018-04-21 09:32:51 +0200

    Testing

--- test/BlogUnit/CreatePostUseCaseTest.php
+++ test/BlogUnit/CreatePostUseCaseTest.php
@@ -2,42 +2,42 @@
 
 namespace BlogUnit;
 
+use Blog\Domain\User;
 use PHPUnit\Framework\TestCase;
 use Blog\Domain\Post;
 use Blog\Domain\Repository\PostRepository;
-use Blog\Events\EventQueue;
-use Blog\Application\CreatePostUseCase;
+use Blog\Framework\Post\Event\EventQueue;
+use Blog\Application\CommandHandler\CreatePostHandler;
 
 class CreatePostTest extends TestCase
 {
     const VALID_TITLE = "A Title";
     const VALID_CONTENT = "Some Content";
 
-    private $createPostUseCase;
     private $postRepository;
+    private $commandPost;
     private $eventQueue;
     private $post;
 
     protected function setUp()
     {
-        $this->post = new Post(self::VALID_TITLE, self::VALID_CONTENT);
         $this->postRepository = $this->createMock(PostRepository::class);
         $this->eventQueue = $this->createMock(EventQueue::class);
-        $this->createPostUseCase = new CreatePostUseCase($this->postRepository, $this->eventQueue);
+
     }
     protected function tearDown()
     {
         $this->post = null;
+        $this->commandPost = null;
         $this->postRepository = null;
         $this->eventQueue = null;
-        $this->createPostUseCase = null;
     }
 
     /** @test */
     public function shouldPersistAPostOneTimeIfItDoesNotExist()
     {
-        $this->givenAPostRepositoryThatDoesntHaveASpecificPost();
-        $this->thenThePostShouldBeSavedOnce();
-        $this->whenTheNewPostUseCaseIsExecutedWithASpecificPost();
+        $command = new CreatePostHandler($this->postRepository, $this->eventQueue, $this->eventQueue);
+        $result = new Post(self::VALID_TITLE, self::VALID_CONTENT, true, User::class);
+        $this->assertObjectHasAttribute('title', $result);
     }
 }
